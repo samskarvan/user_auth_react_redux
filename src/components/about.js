9978d29cd253c9a6307dfa8d90c1152366c340ce
samskarvan <samskarvan@gmail.com>
2018-05-18 15:32:35 -0700

    created nav bar added four pages with appropriate routes

---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default props => (
+    <div>
+    <h1 className="center">this is the ABOUT page</h1>
+    <p>this is a p tag</p>
+    <p>this is a different p tag from the second, distinct component</p>
+    </div>
+)
---
+++ src/components/secret_doc.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export default props => (
+    <div>
+    <h1 className="center">this is for realz a secret</h1>
+    <p>this is a p tag in a top secret doc</p>
+    </div>
+)
